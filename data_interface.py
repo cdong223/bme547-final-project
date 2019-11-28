2db7d62ac4ad1eae3aea4744dd795d671a783a24
Spencer Moavenznadeh <spencer.moavenzadeh@duke.edu>
2019-11-28 15:57:02 -0500

    Included all tabs for UI for further devo within each

--- data_interface.py
+++ data_interface.py
@@ -10,6 +10,20 @@ def data_interface_window(username='NA'):
                  "Application.".format(username))
     window.geometry('500x500')  # Sets window size
 
+    # Create tab control
+    tab_control = ttk.Notebook(window)
+    tab_control.pack(expand=1, fill="both")
+    upload_tab = ttk.Frame(tab_control)
+    display_tab = ttk.Frame(tab_control)
+    download_tab = ttk.Frame(tab_control)
+    metrics_tab = ttk.Frame(tab_control)
+
+    # Label tabs
+    tab_control.add(upload_tab, text="Upload")
+    tab_control.add(display_tab, text="Display")
+    tab_control.add(download_tab, text="Download")
+    tab_control.add(metrics_tab, text="User Metrics")
+
     # Run Window until close
     window.mainloop()
     return
