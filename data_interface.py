b7c13d7512c494a91cf7fdfb0e1142ba57684ee9
Spencer Moavenznadeh <spencer.moavenzadeh@duke.edu>
2019-11-28 19:03:34 -0500

    Upload Tab UI open window to select files for upload

--- data_interface.py
+++ data_interface.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog
+from tkinter import scrolledtext
 
 
 def data_interface_window(username='NA'):
@@ -23,6 +25,35 @@ def data_interface_window(username='NA'):
     tab_control.add(display_tab, text="Display")
     tab_control.add(download_tab, text="Download")
     tab_control.add(metrics_tab, text="User Metrics")
+
+    # Upload tab
+    # Function to choose files wanted to open
+    def choose_files():  # Function for opening Files
+        ftypes = [('Portable Graphics Format .png', '*.png'),
+                  ('Joint Photographic Experts Group .jpeg', '*jpeg'),
+                  ('Tagged Image File Format .tiff', '*.tiff'),
+                  ('Compressed File Format .zip', '*.zip')]
+        files = filedialog.askopenfilenames(filetypes=ftypes)
+        file_display.insert(INSERT, files)
+        return files
+
+    # Choose File Section
+    file_display = scrolledtext.ScrolledText(upload_tab,  # Display files
+                                             width=50,
+                                             height=1)
+    file_display.grid(column=1, row=1)  # Location to display files
+    file_btn = Button(upload_tab,  # Choose files button
+                      text="Choose Files",
+                      bg="white",
+                      fg="black",
+                      command=choose_files)
+    file_btn.grid(column=2, row=1)  # Choose file button location
+
+    # Display tab
+
+    # Download tab
+
+    # User Metrics tab
 
     # Run Window until close
     window.mainloop()
