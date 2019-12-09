e174bd6f4fbb19bcba6cb40c5213effc2cb03f8b
Vivian Qi <vivianqw818@gmail.com>
2019-12-09 11:24:37 -0500

    Fetch image metrics

--- server.py
+++ server.py
@@ -9,12 +9,31 @@ app = Flask(__name__)
 
 
 def database_connection():
-    connect("mongodb+srv://dervil_dong_moavenzadeh_qi:BME54701@cluster0-dykvj.mongodb.net/test?retryWrites=true&w=majority")
+    connect("mongodb+srv://"
+            "dervil_dong_moavenzadeh_qi:BME54701@cluster0-dykvj.mongodb.net/"
+            "test?retryWrites=true&w=majority")
 
 
 # ----------------------------Login Screen--------------------------------
 # -----------------------------Upload tab---------------------------------
 # -----------------------------Display tab--------------------------------
+def find_metrics(id, name):
+    user = UserData.objects.raw({"_id": id}).first()
+    names = user.image_name
+    CPU_times = user.processing_time
+    sizes = user.image_size
+    upload_times = user.upload_dat
+    histograms = user.hist_data
+    for index, item in names:
+        if item == name:
+            CPU_time = CPU_times[index]
+            size = sizes[index]
+            upload_time = upload_times[index]
+            histogram = histograms[index]
+    output_list = [CPU_time, size, upload_time, histogram]
+    return output_list
+
+
 def find_file(image_list, name):
     names = image_list[0]
     files = image_list[1]
@@ -30,6 +49,13 @@ def get_all_images(id):
     image = user.image
     list = [name, image]
     return list
+
+
+@app.route("/api/get_image_metrics/<id>/<name>", methods=["GET"])
+def get_image_metrics(id, name):
+    metrics = find_metrics(id, name)
+    return jsonify(metrics), 200
+
 
 @app.route("/api/fetch_image/<id>/<name>", methods=["GET"])
 def fetch_image(id, name):
