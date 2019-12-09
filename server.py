1e2d3801703853d9b9dba790166496f1d7e17da3
Spencer Moavenznadeh <spencer.moavenzadeh@duke.edu>
2019-12-08 19:45:53 -0500

    Incorporated input json validation to ensure request is what expect

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from pymodm import connect, MongoModel, fields
 from LogIn import LogIn
 from UserData import UserData
 from UserMetrics import UserMetrics
+import logging
 
 app = Flask(__name__)
 
@@ -12,10 +13,75 @@ def database_connection():
     connect("mongodb+srv://dervil_dong_moavenzadeh_qi"
             ":BME54701@cluster0-dykvj.mongodb.net"
             "/test?retryWrites=true&w=majority")
+    logging.basicConfig(filename="image_server.log",
+                        level=logging.INFO,
+                        filemode='w')
 
 
 # ----------------------------Login Screen--------------------------------
 # -----------------------------Upload tab---------------------------------
+def validate_input_json(data, expected):
+    """
+    Validation for input json against expected structure
+
+    Args:
+        data: posted dictionary (dict)
+        expected: expected dictionary structure (dict)
+
+    Returns:
+        valid: state of validity (bool)
+        message: message regarding validity (str)
+        code: status code for validity
+    """
+    # Initialize response assuming all correct
+    valid = True
+    message = "Input json is valid."
+    code = 200  # OK
+
+    # Ensure type data is dict
+    if type(data) != dict:
+        valid = False
+        message = "Data entry is not in dictionary format."
+        code = 400  # Bad Request
+        return valid, message, code
+
+    # Ensure keys in data are same as expected
+    for key in data:
+        if key not in expected:
+            valid = False
+            message = "Dictionary keys are not in correct format."
+            code = 400  # Bad Request
+            return valid, message, code
+    for key in expected:
+        if key not in data:
+            valid = False
+            message = "Dictionary does not have enough " \
+                      "information. Missing keys."
+            code = 400  # Bad Request
+            return valid, message, code
+
+    # Ensure value types in data are same as expected
+    for key in expected:
+        if type(data[key]) not in expected[key]:
+            valid = False
+            message = "Dictionary values are not correct. Invalid data types."
+            code = 400  # Bad Request
+            return valid, message, code
+
+    return valid, message, code
+
+
+@app.route("/api/upload_images", methods=["POST"])
+def upload_images():
+    # Retrieve data sent to server
+    data = request.get_json()  # Returns native dictionary
+
+    # Validate Input json
+    expected = {"username": (str,),
+                "filepaths": (list,)}
+    valid, message, code = validate_input_json(data, expected)
+
+    
 # -----------------------------Display tab--------------------------------
 # ----------------------------Download tab--------------------------------
 # ----------------------------User Metrics tab----------------------------
