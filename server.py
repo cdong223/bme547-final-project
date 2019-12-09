ff9d6d438bb987d60de499a7c8076bd82ac44dfa
Spencer Moavenznadeh <spencer.moavenzadeh@duke.edu>
2019-12-08 19:24:20 -0500

    Encorporated proper connection key for db

--- server.py
+++ server.py
@@ -9,7 +9,9 @@ app = Flask(__name__)
 
 
 def database_connection():
-    connect("DATABASE_KEY")
+    connect("mongodb+srv://dervil_dong_moavenzadeh_qi"
+            ":BME54701@cluster0-dykvj.mongodb.net"
+            "/test?retryWrites=true&w=majority")
 
 
 # ----------------------------Login Screen--------------------------------
