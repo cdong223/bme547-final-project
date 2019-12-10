d3f8b3558b8a559df0656c20dc5b50a516a4e368
Therlking Dervil <therlking.dervil@duke.edu>
2019-12-10 16:49:35 -0500

    Troubleshooting V2

--- Image_Processor_Download.py
+++ Image_Processor_Download.py
@@ -110,31 +110,31 @@ def design_download_window():
             saved_images_listbox = Listbox(saved_images_windows, selectmode = 'multiple', height = len(saved_images), listvariable = saved_images_var)
             saved_images_listbox.grid(column = 2, row = 1, columnspan = 3)
 
-        root_download = Tk()
+    root_download = Tk()
 
-        root_download.title("Image Download")
+    root_download.title("Image Download")
 
-        top_label = ttk.Label(root_download, text="Image Download")
-        top_label.grid(column=2, row=0, columnspan=3)
+    top_label = ttk.Label(root_download, text="Image Download")
+    top_label.grid(column=2, row=0, columnspan=3)
 
-        image_type = StringVar()
-        jpeg_option = ttk.Radiobutton(root_download, text="JPEG", variable=image_type, value="JPEG")
-        jpeg_option.grid(column=2, row=2)
+    image_type = StringVar()
+    jpeg_option = ttk.Radiobutton(root_download, text="JPEG", variable=image_type, value="JPEG")
+    jpeg_option.grid(column=2, row=2)
 
-        png_option = ttk.Radiobutton(root_download, text="PNG", variable=image_type, value="PNG")
-        png_option.grid(column=2, row=3)
+    png_option = ttk.Radiobutton(root_download, text="PNG", variable=image_type, value="PNG")
+    png_option.grid(column=2, row=3)
 
-        tiff_option = ttk.Radiobutton(root_download, text="TIFF", variable=image_type, value="TIFF")
-        tiff_option.grid(column=2, row=4)
+    tiff_option = ttk.Radiobutton(root_download, text="TIFF", variable=image_type, value="TIFF")
+    tiff_option.grid(column=2, row=4)
 
-        download_button = ttk.Button(root_download, text="Download", command=download_button_execution)
-        download_button.grid(column=0, row=5, columnspan=3)
+    download_button = ttk.Button(root_download, text="Download", command=download_button_execution)
+    download_button.grid(column=0, row=5, columnspan=3)
 
-        saved_images_button = ttk.Button(root_download, text="Saved Images", command=saved_images_listbox_execution)
-        saved_images_button.grid(column=1, row=1, columnspan=3)
+    saved_images_button = ttk.Button(root_download, text="Saved Images", command=saved_images_listbox_execution)
+    saved_images_button.grid(column=1, row=1, columnspan=3)
 
-        root_download.mainloop()
-        return
+    root_download.mainloop()
+    return
 
-    if __name__ == "__main__":
-        design_download_window()
+if __name__ == "__main__":
+    design_download_window()
