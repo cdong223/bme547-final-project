fc8674cf90065c3cbb53bf519583ced534ed6719
Spencer Moavenznadeh <spencer.moavenzadeh@duke.edu>
2019-12-11 00:59:31 -0500

    Updated decoding of b64 array to ensure decoded properly

--- server.py
+++ server.py
@@ -349,7 +349,7 @@ def decode_array(array):
         ndarray: decoded array
     """
     # Decoding of 3darray to use for processing
-    decoded_array = base64.b64encode(array)
+    decoded_array = np.frombuffer(base64.b64dencode(array), np.uint8)
     return decoded_array
 
 
